65590989bdd0fd53d7132e40453b23cf4219ff6d
Kim HyunWoo <stories2@naver.com>
2022-08-21 12:53:05 +0900

    sign up

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import LoginView from "./views/LoginView.svelte";
 import StoryBoardView from "./views/StoryBoardView.svelte";
+import SignUpView from "./views/SignUp.svelte";
 
 const routes = {
   "/": LoginView,
   "/story": StoryBoardView,
+  "/signup": SignUpView,
 };
 
 export default routes;
---
+++ src/views/SignUp.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Grid from "svelte-grid-responsive";
+    import { Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
+  import { Button } from 'sveltestrap';
+</script>
+
+<Form>
+    <Grid container gutter={12}>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <FormGroup>
+                <Label for="exampleEmail">Input Email</Label>
+                <Input plaintext type="email" value="Some plain text/ static value"  invalid feedback="I could be wrong" style="border-radius: 5px;
+                border: solid 0.5px rgba(0, 0, 0, 0.1);
+                background-color: #fafafa;"/>
+            </FormGroup>
+        </Grid>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <FormGroup>
+                <Label for="exampleEmail">Input Password</Label>
+                <Input plaintext value="Some plain text/ static value"
+                type="password" style="border-radius: 5px;
+                border: solid 0.5px rgba(0, 0, 0, 0.1);
+                background-color: #fafafa;" />
+            </FormGroup>
+        </Grid>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <Button color="primary" style="width:100%">Sign Up</Button>
+        </Grid>
+    </Grid>
+</Form>
