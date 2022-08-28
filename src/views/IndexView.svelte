d618b300382ee0091be5ebc483c192f561de4c0e
Kim HyunWoo <stories2@naver.com>
2022-08-28 11:51:05 +0900

    Update IndexView.svelte

--- src/views/IndexView.svelte
+++ src/views/IndexView.svelte
@@ -1,8 +1,71 @@
 <script script="ts">
-	export let title = 'Index View'
+    import Grid from "svelte-grid-responsive";
+    import { Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
+  import { Button } from 'sveltestrap';
+  import { FormValidator } from '../controller/form.vaildator';
+  import * as yup from 'yup';
+  import axios from 'axios';
+    const formValues = {
+        email: '',
+        password: '',
+    }
+
+    const formValidator = new FormValidator(
+        {
+            email: yup.string().required("Email is required").matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Not valid email"),
+            password: yup.string().required("Password is required").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/, "Not valid password"),
+        }, formValues
+    );
+
+let errors = {}
+
+const handleSubmit = async (event) => {
+    try {
+        event.preventDefault();
+        console.log('e', event, formValues);
+        const result = await formValidator.validate();
+        console.log('re', result)
+        try {
+            console.log('send');
+            const axiosResult = await axios.post('https://ptsv2.com/t/e999a-1661636501/post', formValues);
+            console.log('ax', axiosResult)
+        } catch (axiosErr) {
+            console.log('axErr', axiosErr);
+        }
+        errors = {};
+    } catch (err) {
+        console.log('err', err.errors, err.path);
+        errors = formValidator.getErrors(err);
+        console.log('errs', errors)
+    }
+}
 </script>
   
-<main>
-	<h1>Hello {title}!</h1>
-	<p>this is index View!</p>
-</main>
+
+<!-- <Grid container> -->
+    <Form on:submit={handleSubmit}>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <FormGroup>
+                <Label for="exampleEmail">Input Email</Label>
+                <!-- https://stackoverflow.com/a/59028650 -->
+                <Input plaintext type="email" bind:value={formValues.email} invalid={errors.email} feedback={errors.email} 
+                style="border-radius: 5px;
+                border: solid 0.5px rgba(0, 0, 0, 0.1);
+                background-color: #fafafa;"/>
+            </FormGroup>
+        </Grid>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <FormGroup>
+                <Label for="exampleEmail">Input Password</Label>
+                <Input plaintext
+                type="password" style="border-radius: 5px;
+                border: solid 0.5px rgba(0, 0, 0, 0.1);
+                background-color: #fafafa;" bind:value={formValues.password} invalid={errors.password} feedback={errors.password}/>
+            </FormGroup>
+        </Grid>
+        <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
+            <Button color="primary" style="width:100%" type="submit">Login</Button>
+        </Grid>
+    </Form>
+
+<!-- </Grid> -->
