ee6ea14725add94767b3a7aa7bf71a4f2bf1e167
Kim HyunWoo <stories2@naver.com>
2022-09-25 02:07:36 +0900

    Update CreatePostView.svelte

--- src/views/CreatePostView.svelte
+++ src/views/CreatePostView.svelte
@@ -31,21 +31,48 @@
         event.preventDefault();
     }
 
-    const handleDrop = (event) => {
+    function fileLoader(file) {
+        return new Promise((resolve) => {
+            let loader = new FileReader();
+            loader.onload = () => {
+                resolve(loader.result);
+            } 
+            loader.readAsDataURL(file)
+        });
+        // https://stackoverflow.com/a/3814285
+    }
+
+    const handleDrop = async (event) => {
         event.preventDefault();
         console.log('e', event.dataTransfer.files);
+        if (event.dataTransfer.files && event.dataTransfer.files.length >= 0) {
+            img = await fileLoader(event.dataTransfer.files[0]);
+        }
     }
+
+    const handleFileInputChange = async (event) => {
+        console.log('e', event.target.files);
+        if (event.target.files && event.target.files.length >= 0) {
+            img = await fileLoader(event.target.files[0]);
+        }
+    }
+    let fileInput;
+    let img = null;
 </script>
 
 <!-- <Grid container> -->
     <Form on:submit={handleSubmit}>
         <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
-            <div style="border: dashed #6c757d 7px;width:100%; height:150px;margin: 15px auto;display: flex;border-radius: 15px;
+            <div style="border: dashed #6c757d 7px;width:100%; height:200px;margin: 15px auto;display: flex;border-radius: 15px;
             align-items: center;
-            justify-content: center;" on:drop={handleDrop} on:dragover={handleDragOver}>
+            background-image: url({img});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            justify-content: center;" on:drop={handleDrop} on:dragover={handleDragOver} on:click={() => {fileInput.click()}}>
                 <Icon src={AiOutlinePlus} size="24"/> 
             </div>
-            <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" style="display: none;"/>
+            <input type="file" id="image" bind:this={fileInput} on:change={handleFileInputChange} name="myImage" accept="image/png, image/gif, image/jpeg" style="display: none;"/>
         </Grid>
         <Grid xs={12} md={10} lg={4} lgOffset={4} mdOffset={1}>
             <FormGroup>
